f4fa2ccf5e1a6eb8b776a7571f4ccfac86faf269
Larissa da Silva Fernandes <l.fernandes0308@gmail.com>
2025-09-03 16:42:02 -0300

    feat: Initial project setup with Spring Boot

---
+++ src/test/java/com/pokemon_world/PokemonWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
